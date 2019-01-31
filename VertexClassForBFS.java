eab38a7ed8d5c141f4159a01aeb9584f449a69e9
Amit Nautiyal <amitnautiyal.cs@gmail.com>
2019-01-31 18:11:48 +0530

    Create VertexClassForBFS.java

---
+++ VertexClassForBFS.java
@@ -0,0 +1,59 @@
+import java.util.List;
+import java.util.LinkedList;
+public class Vertex
+{
+    private int data;
+    private boolean visited;
+    private List<Vertex>neighbourList;
+    
+    public Vertex(int data)
+    {
+        this.data=data;
+        this.neighbourList= new LinkedList<>();
+        
+    }
+    public int getData()
+    {
+        return data;
+        
+    }
+    
+    public void setData(int data)
+    {
+        this.data=data;
+        
+    }
+    public boolean isVisited()
+    {
+        return visited;
+        
+    }
+    public void setVisited(boolean visited)
+    {
+        this.visited=visited;
+    }
+    public List<Vertex> getNeighbourList()
+    {
+        return neighbourList;
+        
+    }
+    public void setNeighbourList(List<Vertex> neighbourList)
+    {
+        this.neighbourList= neighbourList;
+        
+    }
+    
+    public void addNeighbourVertex(Vertex vertex)
+    {
+        
+        this.neighbourList.add(vertex);
+        
+    }
+    @Override
+    public String toString()
+    {
+        return " "+this.data;
+        
+    }
+    
+}
