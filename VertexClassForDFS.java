20625c652103ebd0ea0170900ce90a9d06004b6c
Amit Nautiyal <amitnautiyal.cs@gmail.com>
2019-02-07 15:53:10 +0530

    Create VertexClassForDFS.java

---
+++ VertexClassForDFS.java
@@ -0,0 +1,55 @@
+import java.util.List;
+import java.util.LinkedList;
+public class Vertex
+{
+    private String name;
+    private boolean visited;
+    private List<Vertex>neighbourList;
+    public Vertex(String name)
+    {
+        this.name=name;
+        this.neighbourList= new LinkedList<>();
+    }
+    @Override
+    public String toString()
+    {
+        return this.name;
+        
+    }
+    
+    public String getName()
+    {
+        return name;
+        
+    }
+    public void setName(String name)
+    {
+        
+        this.name=name;
+    }
+    
+     public boolean isVisited()
+    {
+        return visited;
+        
+    }
+    public void setVisited(boolean visited)
+    {
+        this.visited=visited;
+    }
+    public List<Vertex> getNeighbourList()
+    {
+        return neighbourList;
+        
+    }
+    public void setNeighbourList(List<Vertex> neighbourList)
+    {
+        this.neighbourList= neighbourList;
+        
+    }
+    public void addNeighbour(Vertex vertex)
+    {
+        this.neighbourList.add(vertex);
+        
+    }
+}
