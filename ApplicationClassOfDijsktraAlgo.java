b8212798c8206f8d8e390ed9b474f7e8c6e7ab8e
Amit Nautiyal <amitnautiyal.cs@gmail.com>
2019-03-27 11:34:29 +0530

    Create ApplicationClassOfDijsktraAlgo.java
    Application/ Main Class For Dijkstra Algo

---
+++ ApplicationClassOfDijsktraAlgo.java
@@ -0,0 +1,23 @@
+package DijkastraAlgo;
+
+public class App {
+
+	public static void main(String[] args) {
+	
+		Vertex vertex0 = new Vertex("A");
+		Vertex vertex1 = new Vertex("B");
+		Vertex vertex2 = new Vertex("C");
+		
+		vertex0.addNeighbour(new Edge(1, vertex0,vertex1));
+		vertex0.addNeighbour(new Edge(3, vertex0,vertex2));
+		vertex1.addNeighbour(new Edge(1, vertex1,vertex2));
+		
+		DijkstraAlgorithm algo = new DijkstraAlgorithm();
+		algo.computePath(vertex0);
+		System.out.println(algo.getShotestPathTo(vertex2));
+		
+		
+		
+	}
+
+}
